3f05adb3a3a098ac896f90ee5ac787b34ae32827
Javier Mendoza <jmendoza@mindresearch.org>
2022-05-20 02:06:28 -0700

    refactor add button

--- app/src/pages/IndexPage.vue
+++ app/src/pages/IndexPage.vue
@@ -18,20 +18,19 @@
           <div :class="`${$q.dark.isActive ? 'text-white' : 'text-dark'} text-h4 q-pa-md`">
             {{slide.name}}
           </div>
-          <div class="row no-wrap flex full-width q-gutter-x-xs justify-center items-end">
+          <div class="row no-wrap flex full-width justify-center items-end">
 
             <!-- bar graph -->
-            <div v-for="(barValue, barValIndex) in slide.values.slice(-4)" :key="`indexPageCarouselBarVal${slide.values.length - 1 - barValIndex}`" class="column col-2 justify-end" :style="{height: '100%'}">
+            <div v-for="(barValue, barValIndex) in slide.values.slice(-4)" :key="`indexPageCarouselBarVal${slide.values.length - 1 - barValIndex}`" class="column col-2 justify-end q-px-sm" :style="{height: '100%'}">
               <div class="row" :style="{border: '2px solid var(--q-primary)', borderRadius: '4px', height: '80%'}">
                 <div class="bg-primary col self-end" :style="{height: `${Math.min(barValue / Math.max(slide.maxValue, 1.0), 1.0) * 100.0}%`}">&nbsp;</div>
               </div>
               <div class="self-center col" style="font-size: 10px; height: 5%;">{{barValue}} {{slide.measurement}}</div>
             </div>
-            <div class="column col-2 flex-center q-pa-md" :style="{height: '100%'}">
-              <q-btn class="row flex-center" color="secondary">
-                <q-icon name="add"></q-icon>
-              </q-btn>
-              <div class="self-center col" style="font-size: 10px;">ADD</div>
+
+            <!-- add btn -->
+            <div class="row col-2 justify-center items-center q-ml-md" :style="{height: '100%'}">
+              <q-btn icon="add" color="secondary" outline stack></q-btn>
             </div>
           </div>
         </q-card>
@@ -57,7 +56,7 @@ export default defineComponent({
     const slides = ref<Workout[]>([
       {
         name: 'Squats',
-        values: [ 8, 10, 20],
+        values: [ 5, 7, 8, 10, 20],
         maxValue: 20,
         measurement: 'lbs',
       },
